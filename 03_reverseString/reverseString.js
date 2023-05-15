c0d41563e1d8c79c64c97a371305bec2a6f06deb
Oleg Sotnikov <127748313+ole-sot@users.noreply.github.com>
2023-05-15 13:02:26 +0300

    Complete exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    const characters = text.split('');
+    let reversed = '';
+    for (let i = characters.length - 1; i >= 0; i--) {
+        reversed += characters[i];
+    };
+    return reversed;
 };
 
 // Do not edit below this line
