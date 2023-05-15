3238140c3f23483bbe10707daba5a26d60d48c26
Oleg Sotnikov <127748313+ole-sot@users.noreply.github.com>
2023-05-15 11:53:11 +0300

    Complete exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result;
+    if (num > 0) {
+        result = string;
+        for (let i = 1; i < num; i++) {
+            result += string;
+        };
+    } else if (num === 0) {
+        result = '';
+    } else result = 'ERROR';
+    return result;
 };
 
 // Do not edit below this line
