c9ed711faacf31b5bbf2e637e16384e7ab12ff47
Oleg Sotnikov <127748313+ole-sot@users.noreply.github.com>
2023-05-16 12:43:55 +0300

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(userArray = [], ...argumentsToRemove) {
+    for (const arg of argumentsToRemove) {
+        if (userArray.some((element) => element === arg)) {
+            userArray.splice(userArray.indexOf(arg), 1);
+        };
+    };
+    return userArray;
 };
 
 // Do not edit below this line
