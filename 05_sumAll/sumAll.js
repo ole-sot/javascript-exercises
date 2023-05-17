17789517b3a7e3b7389e9d321e19c329b0454c60
Oleg Sotnikov <127748313+ole-sot@users.noreply.github.com>
2023-05-17 14:08:24 +0300

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(from, to) {
+    let sum = 0;
+    if (typeof from === 'number' && typeof to === 'number' && from > 0 && to > 0) {
+       if (from < to) {
+            for (let i = from; i <= to; i++) {
+                sum += i;
+            };
+        } else {
+            for (let i = to; i <= from; i++) {
+                sum += i;
+            }; 
+        }; 
+    } else return 'ERROR';
+    return sum;
 };
 
 // Do not edit below this line
