12c9b883816a31146a03f7f348a4741598c13651
Oleg Sotnikov <127748313+ole-sot@users.noreply.github.com>
2023-05-17 18:51:44 +0300

    Complete exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers = []) {
+  let result = 0;
+  numbers.forEach(
+    (num) => {result += num}
+  );
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers = []) {
+  let result = 1;
+  numbers.forEach(
+    (num) => {result *= num}
+  );
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	return Math.pow(num, pow);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  for (let n = num; n >= 1; n--) {
+    result *= n;
+  };
+  return result;
 };
 
 // Do not edit below this line
