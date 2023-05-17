e695465c57b9ad1390cc85d347598f59d0c7f8fe
Oleg Sotnikov <127748313+ole-sot@users.noreply.github.com>
2023-05-17 16:55:08 +0300

    Complete exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(xDegF) {
+  const xDegC = Math.round(((xDegF - 32) * 5/9) * 10) / 10;
+  return xDegC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(xDegC) {
+  const xDegF = Math.round((xDegC * 9/5 + 32) * 10) / 10;
+  return xDegF;
 };
 
 // Do not edit below this line
