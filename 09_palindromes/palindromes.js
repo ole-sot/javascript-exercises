e443678cf749e188948cbb2c5228e858dd88fe4d
Oleg Sotnikov <127748313+ole-sot@users.noreply.github.com>
2023-05-18 12:36:20 +0300

    Complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
-
+const palindromes = function (inString) {
+    function nonsenseCheck(text) {
+        const array = text.split('');
+        let out
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === array[i+1] && array[i+1] === array[i+2]) {
+              out = true;
+              break;
+            } else out = false;
+        };
+        return out;
+      }
+    function containsSymbol(punctSymbol) {
+        return punctSymbol === '!'
+        || punctSymbol === ' '
+        || punctSymbol === ','
+        || punctSymbol === '.'
+    }
+    
+    if (!nonsenseCheck(inString)) {
+        const filteredStrait = inString.split('').filter(containsSymbol);
+        const filteredReversed = filteredStrait.reverse();
+        return filteredStrait === filteredReversed;
+    } else return false;
 };
 
 // Do not edit below this line
