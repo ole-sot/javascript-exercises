2fbffedd151f30cd671cf48a16f302ac5bc2ec5d
Oleg Sotnikov <127748313+ole-sot@users.noreply.github.com>
2023-05-18 22:34:08 +0300

    Complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibN) {
+    if (fibN <= 0) return 'OOPS';
+    const fibArr = [1, 1];
+    for (let i = 2; i <= fibN; i++) {
+        fibArr.push(fibArr[i - 2] + fibArr[i - 1]);
+    };
+    return fibArr[fibN - 1];
 };
 
 // Do not edit below this line
