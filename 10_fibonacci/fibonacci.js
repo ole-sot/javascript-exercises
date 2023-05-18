d17e016d66ab9dc0fa6898b2b31e84a81eda9afc
Oleg Sotnikov <127748313+ole-sot@users.noreply.github.com>
2023-05-18 22:45:45 +0300

    Optimize 10_fibonacci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,7 +1,7 @@
 const fibonacci = function(fibN) {
     if (fibN <= 0) return 'OOPS';
     const fibArr = [1, 1];
-    for (let i = 2; i <= fibN; i++) {
+    for (let i = 2; i < fibN; i++) {
         fibArr.push(fibArr[i - 2] + fibArr[i - 1]);
     };
     return fibArr[fibN - 1];
