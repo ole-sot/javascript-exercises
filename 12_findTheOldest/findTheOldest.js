fc94ce1b4c50d94dffb7c9da35cc5a0d086b6ca5
Oleg Sotnikov <127748313+ole-sot@users.noreply.github.com>
2023-05-20 13:21:29 +0300

    Complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arrPeople = []) {
+    arrPeople.forEach(obj => {
+        if (obj.yearOfDeath === undefined) {
+            obj['yearToCount'] = new Date().getFullYear();
+        };
+    });
+    function oldestCallback(prevPerson, currPerson) {
+        let oldest;
+        const ageCurrPerson = currPerson.yearOfDeath !== undefined
+        ? (currPerson.yearOfDeath - currPerson.yearOfBirth)
+        : (currPerson.yearToCount - currPerson.yearOfBirth);
+        const agePrevPerson = prevPerson.yearOfDeath !== undefined
+        ? (prevPerson.yearOfDeath - prevPerson.yearOfBirth)
+        : (prevPerson.yearToCount - prevPerson.yearOfBirth);
+        if (agePrevPerson < ageCurrPerson) {
+            oldest = currPerson;
+        } else oldest = prevPerson;
+        return oldest;
+    }
+    return arrPeople.reduce(oldestCallback);
 };
 
 // Do not edit below this line
